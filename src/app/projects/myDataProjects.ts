fc280f569e2a29744c47577d30bab53d4b9adf7b
NikolasT1503 <Nikolas_t@mail.ru>
2021-03-21 03:23:50 +0300

    Добавил мои проекты и полезные ссылки

---
+++ src/app/projects/myDataProjects.ts
@@ -0,0 +1,126 @@
+import { Project } from "./projects.component";
+
+export const PROJECTS: Project[] = [
+    {
+      "lesson": "Введение в Angular",
+      "theme": "Создаем readme.md",
+      "note": "Изучаем Github верстку readme",
+      "href": "https://github.com/NikolasT1503/angulare-homework1",
+      "readme": "https://github.com/NikolasT1503/angulare-homework1/blob/main/README.md"
+    },
+    {
+        "lesson": "Введение в Angular",
+        "theme": "Создаем проект по первому ДЗ",
+        "note": "Изучаем stackblitz.com",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-myproject",
+        "readme": "https://github.com/NikolasT1503/angular-myproject/blob/main/README.md"
+    },
+    {
+        "lesson": "Модули и компоненты. Виды связывания",
+        "theme": "Расчет квадратного уравнения",
+        "note": "Функции и классы",
+        "href": "https://github.com/NikolasT1503/js-project1-app/blob/main/index.js",
+        "readme": "https://github.com/NikolasT1503/js-project1-app/blob/main/README.md"
+    },    
+    {
+        "lesson": "Модули и компоненты. Виды связывания",
+        "theme": "Создание радуги",
+        "note": "Создаем таблицу из одного столбца со всеми цветами радуги. Установка стилей html элементов",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework2-rainbow",
+        "readme": "https://github.com/NikolasT1503/angular-homework2-rainbow/blob/main/README.md"
+    },
+    {
+        "lesson": "Модули и компоненты. Виды связывания",
+        "theme": "Создание 10 компонентов",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework3",
+        "readme": "https://github.com/NikolasT1503/angular-homework3/blob/main/README.md"
+    },
+    {
+        "lesson": "Модули и компоненты. Виды связывания",
+        "theme": "Создание компонентов с разными шаблонами и стилями",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework4",
+        "readme": "https://github.com/NikolasT1503/angular-homework4/blob/main/README.md"
+    },   
+    {
+        "lesson": "Модули и компоненты. Виды связывания",
+        "theme": "Шахматная доска",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework5",
+        "readme": "https://github.com/NikolasT1503/angular-homework5/blob/main/README.md"
+    },
+    {
+        "lesson": "Модули и компоненты. Виды связывания",
+        "theme": "Фибоначи",
+        "note": "Создание компонентов по ряду Фибоначи",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework-fibonachi",
+        "readme": "https://github.com/NikolasT1503/angular-homework-fibonachi/blob/main/README.md"
+    },   
+    {
+        "lesson": "Компоненты. Хуки ЖЦ и шаблонные переменные. Material и Bootstrap",
+        "theme": "Angular Material",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework-material",
+        "readme": "https://github.com/NikolasT1503/angular-homework-material/blob/main/README.md"
+    },
+    {
+        "lesson": "Компоненты. Хуки ЖЦ и шаблонные переменные. Material и Bootstrap",
+        "theme": "Bootstrap",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework-bootstrap/tree/develop",
+        "readme": "https://github.com/NikolasT1503/angular-homework-bootstrap/blob/main/README.md"
+    },   
+    {
+        "lesson": "Пайпы",
+        "theme": "Пример pipe и joinPipe",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework-pipe",
+        "readme": "https://github.com/NikolasT1503/angular-homework-pipe/blob/main/README.md"
+    },
+    {
+        "lesson": "Пайпы",
+        "theme": "Async impure pipe",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework-async-impure-pipe",
+        "readme": "https://github.com/NikolasT1503/angular-homework-async-impure-pipe"
+    },  
+    {
+        "lesson": "Директивы",
+        "theme": "Журнал оценок",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/homework-eJournal",
+        "readme": "https://github.com/NikolasT1503/homework-eJournal/blob/main/README.md"
+    },
+    {
+        "lesson": "Формы",
+        "theme": "Реактивные формы",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework7-editForms",
+        "readme": "https://github.com/NikolasT1503/angular-homework7-editForms/blob/main/README.md"
+    },  
+    {
+        "lesson": "Формы",
+        "theme": "Валидаторы",
+        "note": "",
+        "href": "https://stackblitz.com/github/NikolasT1503/angular-homework7-editForms",
+        "readme": "https://github.com/NikolasT1503/angular-homework7-editForms/blob/main/README.md"
+    },
+    {
+        "lesson": "Dependency Injection",
+        "theme": "Dependency Injection",
+        "note": "",
+        "href": "",
+        "readme": ""
+    },  
+    {
+        "lesson": "Dependency Injection",
+        "theme": "Dependency Injection",
+        "note": "",
+        "href": "",
+        "readme": ""
+    },  
+
+
+
+]
